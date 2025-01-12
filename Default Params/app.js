9d994f47ef60932130d30d49fea2fb869fb4ab52
SaeedKarimian860 <SGK12190@gmail.com>
2025-01-11 20:00:15 -0500

    More Practice

--- Default Params/app.js
+++ Default Params/app.js
@@ -1,3 +1,12 @@
-function rollDie(numSides) {
+//the old way
+// function rollDie(numSides) {
+//     if (numSides === undefined) {
+//         numSides = 6;
+//     }
+//     return Math.floor(Math.random() * numSides) + 1;
+// }
+
+//the new way
+function rollDie(numSides = 6) {
     return Math.floor(Math.random() * numSides) + 1;
 }
